3cf8ff4643984cd6614a9b0b00f4e56f2869c6a6
NilBrains <167288520@qq.com>
2023-03-16 09:00:23 +0800

    change test

--- test/modules/formatSql.js
+++ test/modules/formatSql.js
@@ -1,51 +1,4 @@
-/**
- * 
- * @param {String} sql 
- */
-function formatSql(sql) {
-    const _body = {};
-    sql = sql
-        // 去回车
-        .replace(/[\r\n]/g, "")
-        // 去空格
-        .replace(/\s+/g, ' ')
-        .trim()
-
-    // 正则 /CREATE TABLE '(.*?)'/
-
-    const tableName = sql.match(/CREATE TABLE \'(.*?)\'/);
-    if (tableName == null) {
-        throw new Error("未找到表名称");
-    }
-    _body.tableName = tableName[1];
-
-    const primaryKey = sql.match(/PRIMARY KEY \('(.*?)'\)/);
-    if (primaryKey == null) {
-        throw new Error("未找到表主键");
-    }
-    _body.primaryKey = primaryKey[1];
-
-    const varsSql = sql.match(/' \((.*?) PRIMARY KEY/);
-
-    if (varsSql == null || varsSql == "") {
-        throw new Error("SQL不正确");
-    }
-
-    const varsSqls = varsSql[1].split(",")
-
-    const _vars = []
-    varsSqls.forEach(item => {
-        const field = item.trim()?.match(/^'(.*?)' \w+(\(?(\d+)?\)?)/);
-        const comment = item.trim()?.match(/COMMENT '(.*?)'$/);
-        _vars.push({
-            field: field ? field[1] : '',
-            comment: (comment ? comment[1] : '')
-        })
-    });
-    _body.vars = _vars;
-    return _body
-}
-
+import { formatSql } from "../../src/utils/index.js";
 
 export function main() {
     const sql = `
@@ -65,7 +18,7 @@ export function main() {
     `;
 
 
-    console.log(sql);
+    // console.log(sql);
     const _ = formatSql(sql);
     console.log(_);
 
